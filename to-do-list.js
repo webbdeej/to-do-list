677e6ddc7381923765f1eaf621133d759c5ec85f
Dave Webb <dw2.spiderfex@gmail.com>
2020-09-24 17:38:16 +0100

    Commented out the checked function

--- to-do-list.js
+++ to-do-list.js
@@ -2,29 +2,28 @@ let todo = [];
 
 renderTodoList();
 
-document.getElementById('addBtn').addEventListener('click', function () {
-  let value = document.getElementById('input').value;
+document.getElementById("addBtn").addEventListener("click", function () {
+  let value = document.getElementById("input").value;
   if (value) {
     todo.push(value);
-    saveTodos()
+    saveTodos();
     addInput(value);
   }
 });
 
-input.addEventListener("keypress", function(event) {
-// Number 13 is the "Enter" key on the keyboard
-if (event.keyCode === 13) {
-  // Trigger the button element with a click
-  document.getElementById("addBtn").click();
-}
+input.addEventListener("keypress", function (event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("addBtn").click();
+  }
 });
-
 
 function addInput(text) {
   //add list item on click
-  let listItem = document.createElement('li');
-  let list = document.getElementById('myUL');
-  let input = document.getElementById('input').value;
+  let listItem = document.createElement("li");
+  let list = document.getElementById("myUL");
+  let input = document.getElementById("input").value;
   let textNode = document.createTextNode(text);
 
   //create and append remove button
@@ -36,7 +35,7 @@ function addInput(text) {
   list.appendChild(listItem);
   listItem.appendChild(textNode);
   document.getElementById("input").value = "";
-  removeBtn.addEventListener('click', removeItem);
+  removeBtn.addEventListener("click", removeItem);
   console.log(todo);
 }
 
@@ -45,8 +44,7 @@ function removeItem() {
   let item = this.parentNode.parentNode;
   let parent = item.parentNode;
   let id = parent.id;
-  let value = parent.innerText;
-
+  let value = parent.textContent;
 
   todo.splice(todo.indexOf(value, 1));
   this.parentNode.parentNode.removeChild(this.parentNode);
@@ -54,7 +52,7 @@ function removeItem() {
 }
 
 function renderTodoList() {
-  if (!todo) return
+  if (!todo) return;
   for (let i = 0; i < todo.length; i++) {
     let value = todo[i];
     addInput(value);
@@ -64,30 +62,27 @@ function renderTodoList() {
 
 function saveTodos() {
   let jsonstr = JSON.stringify(todo);
-  localStorage.setItem('todo', jsonstr);
+  localStorage.setItem("todo", jsonstr);
 }
 
 function getTodos() {
-  localStorage.getItem('todo')
+  localStorage.getItem("todo");
   let jsonstr = localStorage.getItem("todo");
   todo = JSON.parse(jsonstr);
   if (!todo || !todo.length) {
     todo = [];
-  }
-  else {
+  } else {
     renderTodoList();
   }
 }
 
-
-
 //cross out text on click
-document.addEventListener('click', function (ev) {
+/*document.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
 
   }
-});
+});*/
 
 //renderTodoList();
 getTodos();
